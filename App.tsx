11068d678e3142c44141099350a8c553c0282c9c
backoi <leebac0802@gmail.com>
2024-11-13 00:42:19 +0700

    auth

--- App.tsx
+++ App.tsx
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import AuthNavigator from "./src/navigation/AuthNavigator";
+import TabNavigator from "./src/navigation/TabNavigator";
+import { HomeScreen, OnBoardingScreen, SplashScreen } from "./src/screens";
+import AppNavigator from "./src/navigation/AppNavigator";
+import { ToastProvider } from 'react-native-toast-notifications'
+import { useAsyncStorage } from "@react-native-async-storage/async-storage";
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <ToastProvider>
+      <NavigationContainer>
+      
+        <AppNavigator/>
+      
+    </NavigationContainer>
+    </ToastProvider>
+  //   isSplash ? (
+  //     <SplashScreen />
+  //   ) : (
+  //     <NavigationContainer>
+  //       <AuthNavigator />
+  //     </NavigationContainer>
+  //   )
+  // );
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  // <OnBoardingScreen/>
+  
+//<AuthNavigator/>
+  
+  
+  
+  )
+}
